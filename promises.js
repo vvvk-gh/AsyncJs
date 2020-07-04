4e0c8a1c7e90370ce3d9d3cc6448d1897b1a7f5c
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-04 16:29:18 +0530

    error-first callbacks functions

--- promises.js
+++ promises.js
@@ -1,59 +1,71 @@
 //Understanding asynchronous js
-
-//write a program that compresses it , download it and uploads it 
-//as our outputs gives an outputs not in a order we execute them so we need to handle them using callbacks
-//adding callbacks 
+//making error first callback function
 
 function download(url , downloaded) {
-    //this console statement will print immediatly as soon as its called
-    //passed string gets copied into url and displays in below statement
     console.log(`Downloading from the ${url} `);
+    if(!url.startsWith('http')){
+       return downloaded(new Error('url needs to start with http'))
+    }
 
     setTimeout(() => {
-    //after 3s the console statement will be printed
         let savedFile = url.split("/").pop();
         console.log(`Dowloaded and saved as ${savedFile}`);
-        // the downloaded here is a callback its sends the savedfile value into the function as we
-        //know the downloaded has a compressed function it will call compress function
-        downloaded(savedFile)
+        //add we need to add err first parameter which will be null  
+        downloaded(null ,savedFile)
     } ,3000)
 }
 function compress(filePath , format , compressed) {
-    //since its called its prints its console statement
-    //filepath is passed from downloaded you can check below calling function and format is copied from compressed fncall in download function
     console.log(`Compressing the file ${filePath}`);
- 
+    if(['zip','fz','gzip'].indexOf(format)=== -1){
+       return compressed(new Error ("We only support zip,fz and gzip formats"))
+    }
     setTimeout(() => {
-        //after 1s console statement will be printed
         let archive = `${filePath.split(".").shift()}.${format}`;
         console.log(`Compressed and saved as ${archive}`)
-        //compressed callback sends the archive as the paramter to upload function
-        compressed(archive)
+        compressed(null, archive)
     } ,1000)
 }
 
 function upload(server , file , uploaded) {
-    //upload gets callled
-    //prints the below immdtly
     console.log(`Uploading ${file} into the ${server}`);
-
+    if(!server.startsWith('ftp://')){
+       return uploaded(new Error("We can upload only to FTP servers"))
+    }
     setTimeout(() => {
-        //after 2s prints the console statement
         let remotePath = `${server}/${file}`
         console.log(`File Uploaded to ${remotePath}`)
-        uploaded(remotePath);
+        uploaded(null, remotePath);
     } ,2000)
 }
-
-//since you have 2 arguments we will pass a string as 1st and a function as 2nd arg
-//dowloads gets called   
-download("https://www.netflix.com/Manu.mp4" , (file) => {
-// we adding a callback inside a callback
-// since compress has 3 arguments we gonna send 3 and 3rd one is an callbackfunction    
-// here we are using the same file
-compress(file,'zip', (archive)=>{
-     //calling upload
-     upload('https://file.com', archive ,() => {})
+// need to change the format as we are using error first callback format
+download("https://www.netflix.com/Manu.mp4" , (err ,file) => {
+if(err) throw err;
+compress(file,'zip', (err, archive)=>{
+    //we the format doesn't match we can send the compressed file 
+    if (err){
+        console.warn(err);
+        archive = file;
+     }
+     upload('ftp://file.com', archive ,(err) => {
+         if(err) throw err;
+     })
     })
 });
 
+
+//example of how it works
+// task1( arg1 , (res1)=>{
+//     task2(arg2 ,(res2)=>{
+//         task3(args3 , (err,res3)=>{
+
+//         })
+//     })
+// })
+//since its error first callback adding err,callback
+// task1(arg1 , (err1,data1)=>{
+//     task2(arg2 ,(err2,data2)=>{
+//         task3(args3 , (err3,data3)=>{
+
+//         })
+//     })
+// })
